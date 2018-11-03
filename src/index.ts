7a1711c5515c9b0fdb0a7213c6033e84f5cab711
SkyLake <abhishek.kumar.k@icloud.com>
2018-11-03 19:32:01 +0530

    Added backend code for web socket

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import * as app from './app';
 import * as express from 'express';
 import { createServer } from 'http';
 import { Server } from "socket.io";
+import SocketApp from './socket/socket';
 
 const server = createServer(app.expressApp)
+const s = new SocketApp(server);
 
 server.listen('4300', () => {
     console.log('Listening at 4300');
-}); 
-
-/* app.expressApp.listen('4300', () => {
-    console.log('Listening at 4300');
-}); */
+});
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,9 +1,35 @@
-import { Server } from "socket.io";
+import { Server } from "http";
+import * as socketIo from 'socket.io';
 
-class SocketApp {
-    private io: Server;
+export default class SocketApp {
+    private io: SocketIO.Server;
 
-    constructor(socketServer: Server) {
-        this.io = socketServer;
+    constructor(server: Server) {
+        this.io = socketIo(server);
+
+        this.listen();
+    }
+
+    private listen() {
+        this.io.on('connect', this.onClientConnected.bind(this));
+    }
+
+    private onClientConnected(socket: any) {
+        console.log('Connected client');
+        socket.on('message', this.onMessageReceived.bind(this));
+        socket.on('disconnect', this.onClientDisconnected.bind(this));
+    }    
+
+    private onMessageReceived(message: any) {
+        console.log('Received Message: %s', JSON.stringify(message));
+        this.sendMessage(message);
+    }
+
+    private sendMessage(message: any) {
+        const n = this.io.emit('message', message);
+    }
+
+    private onClientDisconnected() {
+        console.log('Client disconnected');
     }
 }
