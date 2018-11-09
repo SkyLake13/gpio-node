0fcb6583b349acfc4f8c06e91b6644ff0fb83c94
SkyLake <abhishek.kumar.k@icloud.com>
2018-11-09 22:46:49 +0530

    minor changes

--- src/models/switch.ts
+++ src/models/switch.ts
@@ -1,33 +1,37 @@
-import { Gpio } from "onoff";
+// import { Gpio } from "onoff";
 import { setTimeout } from "timers";
 
 export class Switch {
-    /* private _state: number;
+    private _state: number;
     set state(value: number) {
         this._state = value;
     }
     get state(): number {
         return this._state;
-    } */
+    }
 
-    private gp: Gpio
+    /* private gp: Gpio
     set state(value: number) {
         this.gp.writeSync(value);
     } 
     get state(): number {
         return this.gp.readSync();
-    }
+    } */
 
     constructor(public name: string, public url: string, 
         gpio: number, state: number, public timeout?: number) {
-        this.gp = new Gpio(gpio, "out");
+        // this.gp = new Gpio(gpio, "out");
         this.state = state;
     }
 
     public on() {
         this.state = 1;
 
-        this.switchTimeout(this.timeout);
+        if(this.timeout) {
+            this.timeout = 10000;
+            this.switchTimeout(this.timeout);
+        }
+        
     }
 
     public off() {
@@ -35,17 +39,15 @@ export class Switch {
     }
 
     private switchTimeout(timeout: number) {
-        if(timeout) {
             const interval = 1000;
             setInterval(() => {
-                this.timeout = this.timeout - interval;
-    
                 if (this.timeout < 1000) {
                     if (this.state === 1) {
                         this.off();
                     }
+                } else {
+                    this.timeout = this.timeout - interval;
                 }
             }, interval);
-        }
     }
 }
